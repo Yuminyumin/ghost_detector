4227cc1af8fe67eecf4cd2b0779eab9f8f691af2
Yuming <102581107+Yuminyumin@users.noreply.github.com>
2023-09-16 02:36:40 +0900

    feat : 초기
    S3 로드
    전처리 및 병합
    확률 계산 함수
    람다 핸들러

---
+++ detector.py
@@ -0,0 +1,86 @@
+# 가정: closed_school_df, closed_house_df, memorial_park_df는 각각 폐교, 폐가, 추모공원 데이터셋을 담고 있는 DataFrame 객체
+# mortality_df는 사망률 데이터셋을 담고 있는 DataFrame 객체
+import boto3
+import pandas as pd
+from io import StringIO
+
+s3 = boto3.client('s3')
+
+def load_data_from_s3(bucket, key):
+    obj = s3.get_object(Bucket=bucket, Key=key)
+    data = obj['Body'].read().decode('utf-8')
+    return pd.read_csv(StringIO(data))
+
+# 예시: bucket_name과 file_name은 실제 값으로 바꿔야 함
+closed_school_df = load_data_from_s3(bucket_name, 'closed_school.csv')
+closed_house_df = load_data_from_s3(bucket_name, 'closed_house.csv')
+memorial_park_df = load_data_from_s3(bucket_name, 'memorial_park.csv')
+mortality_df = load_data_from_s3(bucket_name, 'mortality.csv')
+
+
+# 각 데이터 셋에 카운트 열 추가
+closed_school_df['school_count'] = 1
+closed_house_df['house_count'] = 1
+memorial_park_df['park_count'] = 1
+
+# 'province', 'city', 'district'를 기준으로 각각 그룹화 후 카운트 계산
+school_grouped = closed_school_df.groupby(['province', 'city', 'district'])['school_count'].count().reset_index()
+house_grouped = closed_house_df.groupby(['province', 'city', 'district'])['house_count'].count().reset_index()
+park_grouped = memorial_park_df.groupby(['province', 'city', 'district'])['park_count'].count().reset_index()
+
+# 세 개의 그룹화된 데이터 셋 병합
+merged_1 = pd.merge(school_grouped , house_grouped , on=['province','city','district'], how='outer')
+merged_total = pd.merge(merged_1 , park_grouped , on=['province','city','district'], how='outer')
+
+# NaN 값 처리 (NaN은 해당 지역에서 관련 시설이 없음을 의미)
+merged_total.fillna(0, inplace=True)
+
+# 마지막으로 사망률 데이터 셋 병합 ('how' 파라미터를 사용하여 필요한 방식의 조인 수행)
+final_merged_data= pd.merge(merged_total,mortality_df,on='province',how='inner')
+
+# province를 기준으로 그룹화 후 die 열의 평균 계산
+mortality_avg = mortality_df.groupby('province')['die'].mean().reset_index()
+
+# 마지막으로 평균 사망률 데이터 셋 병합
+final_merged_data= pd.merge(merged_total,mortality_avg,on='province',how='inner')
+
+def calculate_probability(df, province, city, district):
+    # 해당 지역의 데이터 행 찾기
+    row = df[(df['province'] == province) & (df['city'] == city) & (df['district'] == district)]
+    
+    if len(row) == 0:
+        return "해당 위치에 대한 데이터가 없습니다."
+    
+    # 폐교, 폐가, 추모공원 개수 및 사망률 값을 가져옴
+    school_count = row['school_count'].values[0]
+    house_count = row['house_count'].values[0]
+    park_count = row['park_count'].values[0]
+    mortality_rate = row['die'].values[0]  # 'die' 열이 사망률을 나타낸다고 가정
+    
+    # 가중치 적용: 사망률 4점, 폐교/폐가/추모공원 각각 2점
+    weighted_total = mortality_rate * 4 + school_count * 2 + house_count * 2 + park_count * 2
+
+    # 정규화 및 퍼센트 변환: 전체 점수를 가중치 합계로 나눈 후 x100
+    probability_percent = (weighted_total / (4+2+2+2)) * 100
+    
+    return probability_percent
+
+def lambda_handler(event, context):
+    province = event['province']
+    city = event['city']
+    district = event['district']
+
+    # calculate_probability 함수 호출
+    probability_percent = calculate_probability(final_merged_data , province , city , district)
+
+    # 결과 반환 (문자열 형태라면 오류 처리 필요)
+    if isinstance(probability_percent,str):
+        return {
+            'statusCode': 400,
+            'body': probability_percent
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': f"귀신이 나타날 확률은 {probability_percent}%입니다."
+        }
