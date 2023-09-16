201f64d9d9d9c3a79609f6f913b183ee795bc8c8
Yuming <102581107+Yuminyumin@users.noreply.github.com>
2023-09-16 22:25:32 +0900

    fix : 가중치 (로컬 최종)
    가중치
    사망률 1
    추모공원 1
    폐교 4
    폐가 4
    로컬 최종

--- detector.py
+++ detector.py
@@ -29,6 +29,11 @@ mortality_avg= mortality_df.groupby('province')['die'].mean().reset_index()
 # 마지막으로 평균 사망률 데이터 셋 병합 
 final_merged_data= pd.merge(merged_total,mortality_avg,on=['province'],how='inner')
 
+for column in ['school_count', 'house_count', 'park_count']:
+    min_val = final_merged_data[column].min()
+    max_val = final_merged_data[column].max()
+    final_merged_data[f'scaled_{column}'] =(final_merged_data[column] - min_val) / (max_val - min_val)
+
 def calculate_probability(df, province, city, district):
     # 해당 지역의 데이터 행 찾기
     row=df[(df["province"]== province) & (df["city"]== city) & (df["district"]== district)]
@@ -37,17 +42,17 @@ def calculate_probability(df, province, city, district):
         return "해당 위치에 대한 데이터가 없습니다."
     
     # 폐교, 폐가, 추모공원 개수 및 사망률 값을 가져옴
-    school_count=row["school_count"].values[0]
-    house_count=row["house_count"].values[0]
-    park_count=row["park_count"].values[0]
+    school_count=row["scaled_school_count"].values[0]
+    house_count=row["scaled_house_count"].values[0]
+    park_count=row["scaled_park_count"].values[0]
 
-    mortality_rate=row["die"].values[0]  
+    mortality_rate=row['die'].values[0]  
     
-    # 가중치 적용: 사망률 4점, 폐교/폐가/추모공원 각각 2점
-    weighted_total=mortality_rate*10+school_count+house_count+park_count
+     # 가중치 적용: 사망률 4점, 폐교/폐가/추모공원 각각 2점
+    weighted_total=mortality_rate*1+school_count*4+house_count*4+park_count*1
 
-    # 정규화 및 퍼센트 변환: 전체 점수를 가중치 합계로 나눈 후 x100
-    probability_percent=(weighted_total/13)*100
+     # 정규화 및 퍼센트 변환: 전체 점수를 가중치 합계로 나눈 후 x100
+    probability_percent=(weighted_total/10)*100
     
     return probability_percent
 
