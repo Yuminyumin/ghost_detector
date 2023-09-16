ee1d0146533c5f41aa339de6195368c7f0e73928
Yuming <102581107+Yuminyumin@users.noreply.github.com>
2023-09-16 22:04:30 +0900

    fix : 스케일링 전
    스케일링 전
    가중치 10,1,1,1

--- detector.py
+++ detector.py
@@ -1,10 +1,7 @@
-# 가정: closed_school_df, closed_house_df, memorial_park_df는 각각 폐교, 폐가, 추모공원 데이터셋을 담고 있는 DataFrame 객체
-# mortality_df는 사망률 데이터셋을 담고 있는 DataFrame 객체
-
 import pandas as pd
 
 # CSV 파일에서 데이터 불러오기
-closed_school_df =pd.read_csv('/Users/yuming/ghost_data/closed_school.csv',  encoding='cp949')
+closed_school_df = pd.read_csv('/Users/yuming/ghost_data/closed_school.csv',  encoding='cp949')
 closed_house_df = pd.read_csv('/Users/yuming/ghost_data/closed_house.csv',  encoding='cp949')
 memorial_park_df = pd.read_csv('/Users/yuming/ghost_data/memorial_park.csv',  encoding='cp949')
 mortality_df = pd.read_csv('/Users/yuming/ghost_data/mortality.csv',  encoding='cp949')
@@ -26,40 +23,42 @@ merged_total = pd.merge(merged_1 , park_grouped , on=['province','city','distric
 # NaN 값 처리 (NaN은 해당 지역에서 관련 시설이 없음을 의미)
 merged_total.fillna(0, inplace=True)
 
-# 마지막으로 사망률 데이터 셋 병합 ('how' 파라미터를 사용하여 필요한 방식의 조인 수행)
-final_merged_data= pd.merge(merged_total,mortality_df,on='province',how='inner')
-
 # province를 기준으로 그룹화 후 die 열의 평균 계산
-mortality_avg = mortality_df.groupby('province')['die'].mean().reset_index()
+mortality_avg= mortality_df.groupby('province')['die'].mean().reset_index()
 
-# 마지막으로 평균 사망률 데이터 셋 병합
-final_merged_data= pd.merge(merged_total,mortality_avg,on='province',how='inner')
-
-# 사용자로부터 지역 정보 입력 받기
-province = input("도를 입력하세요: ")
-city = input("시를 입력하세요: ")
-district = input("동을 입력하세요: ")
+# 마지막으로 평균 사망률 데이터 셋 병합 
+final_merged_data= pd.merge(merged_total,mortality_avg,on=['province'],how='inner')
 
 def calculate_probability(df, province, city, district):
     # 해당 지역의 데이터 행 찾기
-    row = df[(df['province'] == province) & (df['city'] == city) & (df['district'] == district)]
+    row=df[(df["province"]== province) & (df["city"]== city) & (df["district"]== district)]
     
-    if len(row) == 0:
+    if len(row)==0:
         return "해당 위치에 대한 데이터가 없습니다."
     
     # 폐교, 폐가, 추모공원 개수 및 사망률 값을 가져옴
-    school_count = row['school_count'].values[0]
-    house_count = row['house_count'].values[0]
-    park_count = row['park_count'].values[0]
-    mortality_rate = row['die'].values[0]  # 'die' 열이 사망률을 나타낸다고 가정
+    school_count=row["school_count"].values[0]
+    house_count=row["house_count"].values[0]
+    park_count=row["park_count"].values[0]
+
+    mortality_rate=row["die"].values[0]  
     
     # 가중치 적용: 사망률 4점, 폐교/폐가/추모공원 각각 2점
-    weighted_total = mortality_rate * 4 + school_count * 2 + house_count * 2 + park_count * 2
+    weighted_total=mortality_rate*10+school_count+house_count+park_count
 
     # 정규화 및 퍼센트 변환: 전체 점수를 가중치 합계로 나눈 후 x100
-    probability_percent = (weighted_total / (4+2+2+2)) * 100
+    probability_percent=(weighted_total/13)*100
     
     return probability_percent
 
-probability_percent= calculate_probability(final_merged_data, province, city, district)
-print(f"귀신이 나타날 확률은 {probability_percent}입니다.")
+# 사용자로부터 지역 정보 입력 받기
+province=input("도를 입력하세요: ")
+city=input("시를 입력하세요: ")
+district=input("동을 입력하세요: ")
+
+probability_percent=calculate_probability(final_merged_data, province, city, district)
+
+if isinstance(probability_percent,str):
+   print(probability_percent)
+else:
+   print(f"귀신이 나타날 확률은 {probability_percent}%입니다.")
