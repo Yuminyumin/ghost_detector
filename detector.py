b3d6b7630cbfa2189b01e82c3fd01867d592b222
Yuming <102581107+Yuminyumin@users.noreply.github.com>
2023-09-17 13:42:44 +0900

    Update detector.py

--- detector.py
+++ detector.py
@@ -26,7 +26,7 @@ merged_total.fillna(0, inplace=True)
 # province를 기준으로 그룹화 후 die 열의 평균 계산
 mortality_avg= mortality_df.groupby('province')['die'].mean().reset_index()
 
-# 마지막으로 평균 사망률 데이터 셋 병합 
+# 마지막으로 평균 사망률 데이터셋 병합 
 final_merged_data= pd.merge(merged_total,mortality_avg,on=['province'],how='inner')
 
 for column in ['school_count', 'house_count', 'park_count']:
@@ -45,7 +45,6 @@ def calculate_probability(df, province, city, district):
     school_count=row["scaled_school_count"].values[0]
     house_count=row["scaled_house_count"].values[0]
     park_count=row["scaled_park_count"].values[0]
-
     mortality_rate=row['die'].values[0]  
     
      # 가중치 적용: 사망률 4점, 폐교/폐가/추모공원 각각 2점
